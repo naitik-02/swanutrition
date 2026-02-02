31062ffa143ebeccfc18ed6fd19b7a3fadefb2aa
naitik <singhnaitik900@gmail.com>
2026-02-02 13:33:59 +0530

    commit

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,6 +1,7 @@
 "use client";
 
 import AdminShell from "@/components/Layout/AdminShell.jsx";
+import "react-quill-new/dist/quill.snow.css";
 
 export default function AdminLayout({ children }) {
   return <AdminShell>{children}</AdminShell>;
